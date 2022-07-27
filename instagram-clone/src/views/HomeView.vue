5c40a51c2ceb790ee5f862fc3b9a18bccf9fc43a
talhayavcin <talhayavcin35@gmail.com>
2022-07-27 23:39:28 +0300

    css ayarları ve diğer ayarlar yapıldı

---
+++ instagram-clone/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <CustomText>Merhaba DÜnya</CustomText>
+  </div>
+</template>
+
+<script>
+import CustomText from '@/components/CustomText'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CustomText
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+</style>

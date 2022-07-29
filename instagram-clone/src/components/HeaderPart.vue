bc5cfb4ee4d7b13ee9c29690ca21065356da9ffe
talhayavcin <talhayavcin35@gmail.com>
2022-07-29 17:05:07 +0300

    iconlar eklendi

--- instagram-clone/src/components/HeaderPart.vue
+++ instagram-clone/src/components/HeaderPart.vue
@@ -1,15 +1,29 @@
 <template>
-  <header>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/direct">Direct</router-link> |
-    <router-link to="/explore">Explore</router-link> |
+  <header class="header">
+    <router-link to="/"> Instagram </router-link>
+    <form>
+      <input type="text" placeholder="Search" />
+    </form>
+    <router-link to="/">
+      <IconHome />
+      <IconHomeFill />
+    </router-link>
+    <router-link to="/direct">Direct</router-link>
+    <router-link to="/explore">Explore</router-link>
     <router-link to="/profile">Profile</router-link>
   </header>
 </template>
 
 <script>
+import IconHome from '@/icons/home.svg'
+import IconHomeFill from '@/icons/home-fill.svg'
+
 export default {
-  name: 'MainHeader'
+  name: 'MainHeader',
+  components: {
+    IconHome,
+    IconHomeFill
+  }
 }
 </script>
 

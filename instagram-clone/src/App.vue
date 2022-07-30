6be19c8856bd6bafc92caa9a2652ddc9b01dbde1
talhayavcin <talhayavcin35@gmail.com>
2022-07-30 13:24:44 +0300

    header componenti tasarlandı

--- instagram-clone/src/App.vue
+++ instagram-clone/src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="app">
     <MasterHeader />
-    <router-view />
+
+    <main class="main">
+      <ContainerPart>
+        <router-view />
+      </ContainerPart>
+    </main>
   </div>
 </template>
 
 <script>
 import MasterHeader from '@/components/HeaderPart'
+import ContainerPart from './components/ContainerPart.vue'
 export default {
   name: 'App',
   components: {
-    MasterHeader
+    MasterHeader,
+    ContainerPart
   }
 }
 </script>
+
+<style scoped>
+.main {
+  padding-top: 40px;
+}
+</style>
--- instagram-clone/src/components/HeaderPart.vue
+++ instagram-clone/src/components/HeaderPart.vue
@@ -1,30 +1,84 @@
 <template>
   <header class="header">
-    <router-link to="/"> Instagram </router-link>
-    <form>
-      <input type="text" placeholder="Search" />
-    </form>
-    <router-link to="/">
-      <IconHome />
-      <IconHomeFill />
-    </router-link>
-    <router-link to="/direct">Direct</router-link>
-    <router-link to="/explore">Explore</router-link>
-    <router-link to="/profile">Profile</router-link>
+    <ContainerPart class="header-inner">
+      <router-link class="logo" to="/"> Instagram </router-link>
+      <form class="search">
+        <input class="input" type="text" placeholder="Search" />
+      </form>
+      <nav class="navigation">
+        <router-link to="/">
+          <IconHomeFill v-if="$route.name === 'Home'" />
+          <IconHome v-else />
+        </router-link>
+        <router-link to="/direct">
+          <IconDirectFill v-if="$route.name === 'Direct'" />
+          <IconDirect v-else />
+        </router-link>
+        <router-link to="/explore">
+          <IconExploreFill v-if="$route.name === 'Explore'" />
+          <IconExplore v-else />
+        </router-link>
+        <router-link to="/profile">Profile</router-link>
+      </nav>
+    </ContainerPart>
   </header>
 </template>
 
 <script>
 import IconHome from '@/icons/home.svg'
 import IconHomeFill from '@/icons/home-fill.svg'
+import IconDirect from '@/icons/direct.svg'
+import IconDirectFill from '@/icons/direct-fill.svg'
+import IconExplore from '@/icons/explore.svg'
+import IconExploreFill from '@/icons/explore-fill.svg'
+import ContainerPart from '@/components/ContainerPart'
 
 export default {
   name: 'MainHeader',
   components: {
     IconHome,
-    IconHomeFill
+    IconHomeFill,
+    IconDirect,
+    IconDirectFill,
+    IconExplore,
+    IconExploreFill,
+    ContainerPart
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  border-bottom: 1px solid rgb(var(--b6a));
+}
+.header-inner {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  height: 60px;
+}
+.logo {
+  font-size: 2rem;
+  font-weight: bold;
+}
+.search {
+  text-align: center;
+}
+.input {
+  width: 215px;
+  height: 28px;
+  border: 1px solid rgb(var(--b6a));
+  border-radius: 4px;
+  background-color: rgb(var(--b3f));
+  padding-left: 20px;
+  padding-right: 20px;
+}
+.navigation {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.navigation > a {
+  margin-left: 20px;
+}
+</style>
